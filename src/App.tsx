655b6ad06eb6b043e0742d4be1c2963a756464d6
Ayako <ayakonagane@gmail.com>
2024-08-10 19:47:28 +1200

    made everything typesctipt

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, useRef, RefObject } from "react";
+import "./App.css";
+import Pipe from "./components/Pipe";
+import Ground from "./components/Ground";
+import Coin from "./components/Coin";
+import Player from "./components/Player";
+import usePlayerMovement from "./hooks/usePlayerMovement";
+import useScrollEffect from "./hooks/useScrollEffect";
+
+const App = () => {
+  //so that Everytime key is pressed, handle keydown is triggred and changes position
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  //import use state and function from use hook
+  const { playerPosition, handleKeyDown } = usePlayerMovement();
+
+  // References to the player element and the container
+  const playerRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null); // reference to the player in the DOM
+  const containerRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null); // reference to the container in the DOM
+
+  // calling hook useScrollEffect, everytime App.js is rendered this is called and triggers useEffect inside the hook
+  useScrollEffect(playerPosition, containerRef, playerRef);
+
+  return (
+    <div className="game-container" ref={containerRef}>
+      <div className="background-grid">
+        {/* Add ground, pipe, and coin elements */}
+        <Ground />
+
+        <Pipe />
+        <Coin />
+        <Player playerPosition={playerPosition} playerRef={playerRef} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hooks/useScrollEffect.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useEffect, RefObject } from "react";
+
+interface PlayerPosition {
+  x: number;
+  y: number;
+}
+
+function useScrollEffect(
+  playerPosition: PlayerPosition,
+  containerRef: RefObject<HTMLDivElement>,
+  playerRef: RefObject<HTMLDivElement>
+) {
+  // 1.Scroll the container to keep the player centered
+
+  useEffect(() => {
+    // useEffect can directly manupilate DOM
+    if (containerRef.current && playerRef.current) {
+      // access to container element
+      const containerWidth = containerRef.current.offsetWidth; // full width of the container
+      const playerElement: HTMLDivElement | null = playerRef.current;
+
+      const playerOffset =
+        playerElement.offsetLeft + playerElement.offsetWidth / 2;
+      const scrollOffset = containerWidth / 2;
+      containerRef.current.scrollLeft = playerOffset - scrollOffset;
+
+      // this fixes the camera to opsition the character in the center
+      //playerOffSet and scrollOffset are equal
+    }
+  }, [playerPosition]);
+}
+
+export default useScrollEffect;
