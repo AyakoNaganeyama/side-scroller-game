a966732b7b62ac8c46933409dc8f7b1a46ebea29
multiahfoon <multiahfoon@gmail.com>
2024-10-03 11:55:39 +1300

    docs: add more information hook logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ import { usePlayerMovement } from './hooks/usePlayerMovement'
 import { useScrollEffect } from './hooks/useScrollEffect'
 
 export default function App() {
+	// player position is managed as soon as app launches
+	// this includes all arrow key input action listeners
 	const { playerPosition } = usePlayerMovement()
 	// target dom elements
 	const playerRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
 	const cameraRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
 
-	// track and update scroll
+	// fires as soon as app launches, tracks and update scroll
 	useScrollEffect(playerPosition, cameraRef, playerRef)
 
 	return (
