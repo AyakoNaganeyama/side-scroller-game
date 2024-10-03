c51292112979492a8fb1279a690f8254c64c68eb
multiahfoon <multiahfoon@gmail.com>
2024-10-03 13:12:31 +1300

    feat: add constant variables for the game

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+// global game fixed values
+export const TOTAL_COLUMNS = 100 // set the width of the map
+export const TOTAL_ROWS = 10 // set game sky height
+
+export const MAX_X = TOTAL_COLUMNS - 1 // set right side boundary of map so player doesn't pop out of map when reaching the end
+export const MIN_X = 0 // same as above set left side boundary so player doesn't pop out of the game when running back to start
+export const MAX_Y = TOTAL_ROWS - 1 // bottom of screen because 0 is top left of the screen in grid css, stand html 0,0 origin point is top left
+export const MIN_Y = 0 // top side boundary, prevent player from going past sky box
