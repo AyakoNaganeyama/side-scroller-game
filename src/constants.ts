f5e4b3d674550a4655cff59d79908674ea484e3d
multiahfoon <multiahfoon@gmail.com>
2024-10-07 17:23:24 +1300

    feat: add global ground level row

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 // global game fixed values
 export const TOTAL_COLUMNS = 100 // set the width of the map
 export const TOTAL_ROWS = 10 // set game sky height
+export const GROUND_ROW = 8
 
 export const MAX_X = TOTAL_COLUMNS - 1 // set right side boundary of map so player doesn't pop out of map when reaching the end
 export const MIN_X = 0 // same as above set left side boundary so player doesn't pop out of the game when running back to start
