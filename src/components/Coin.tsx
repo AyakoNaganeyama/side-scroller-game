50fef80384789573b1420ced834284452b1e29f0
multiahfoon <multiahfoon@gmail.com>
2024-10-08 20:12:34 +1300

    feat: add coin visible toggling on collision

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,4 +1,16 @@
-export function Coin({ column, row }: { column: string; row: string }) {
+import { useEffect, useState } from 'react'
+import { PlayerPosition } from '../hooks/usePlayerMovement'
+import { COIN_COLLISION } from '../constants'
+
+type CoinType = {
+	column: string
+	row: string
+	playerPosition: PlayerPosition
+}
+
+export function Coin({ column, row, playerPosition }: CoinType) {
+	const [isVisible, setIsVisible] = useState<boolean>(true)
+
 	const style = {
 		sprite: {
 			backgroundColor: 'yellow' /* color for now can replace later */,
@@ -8,5 +20,17 @@ export function Coin({ column, row }: { column: string; row: string }) {
 		position: { gridColumn: `${column} / span 1`, gridRow: `${row} / span 1` },
 	}
 
-	return <div style={{ ...style.sprite, ...style.position }} />
+	useEffect(() => {
+		if (COIN_COLLISION[`X${playerPosition.x}Y${playerPosition.y}`])
+			setIsVisible(false)
+	}, [playerPosition])
+
+	return isVisible ? (
+		<div
+			style={{
+				...style.sprite,
+				...style.position,
+			}}
+		/>
+	) : null
 }
