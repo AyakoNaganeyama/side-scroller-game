4e290e69da8328a289e705ef05974f2f3406bbea
multiahfoon <multiahfoon@gmail.com>
2024-10-08 13:28:44 +1300

    feat: add column and rows params
    this allows for reuse of the same component

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,12 +1,12 @@
-export function Coin() {
-	return <div style={{ ...style.sprite, ...style.position }} />
-}
+export function Coin({ column, row }: { column: string; row: string }) {
+	const style = {
+		sprite: {
+			backgroundColor: 'yellow' /* color for now can replace later */,
+			border: '1px solid white',
+			borderRadius: '50%',
+		},
+		position: { gridColumn: `${column} / span 1`, gridRow: `${row} / span 1` },
+	}
 
-const style = {
-	sprite: {
-		backgroundColor: 'yellow' /* color for now can replace later */,
-		border: '1px solid white',
-		borderRadius: '50%',
-	},
-	position: { gridColumn: '15 / span 1', gridRow: '5 / span 1' },
+	return <div style={{ ...style.sprite, ...style.position }} />
 }
