5aa9009b628ddd6b07b5c5970d85d1e15b677e42
multiahfoon <multiahfoon@gmail.com>
2024-10-09 18:15:28 +1300

    fix: missing display grid

--- src/components/Pipe.tsx
+++ src/components/Pipe.tsx
@@ -4,6 +4,7 @@ export function Pipe({ column }: { column: string }) {
 	return (
 		<div
 			style={{
+				display: 'grid',
 				gridColumn: `${column} / span 1`,
 				gridRow: '8 / span 2',
 				...style.sprite,
