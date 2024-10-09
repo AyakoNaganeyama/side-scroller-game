94b331c6671359397b32f0213ac90a454d677ac2
multiahfoon <multiahfoon@gmail.com>
2024-10-09 18:15:02 +1300

    feat: add column param

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,14 +1,12 @@
-import '../App.css'
+export function Ground({ column }: { column: string }) {
+	const style = {
+		sprite: {
+			backgroundColor: 'brown' /* color for now can use sprite later */,
+			border: '1px solid white' /* for testing grid */,
+			display: 'grid',
+		},
+		position: { gridColumn: `${column}`, gridRow: '10 / span 1' },
+	}
 
-export function Ground() {
 	return <div style={{ ...style.sprite, ...style.position }}></div>
-}
-
-const style = {
-	sprite: {
-		backgroundColor: 'brown' /* color for now can use sprite later */,
-		border: '1px solid white' /* for testing grid */,
-		gridColumn: '1 / -1' /* ground position on grid */,
-	},
-	position: { gridColumn: '1 / -1', gridRow: '10 / span 1' },
 }
